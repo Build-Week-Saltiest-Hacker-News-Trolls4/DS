c38f639c811558899e2214cb309ba5d83b038873
Tally Wiesenberg <t.j.wies@gmail.com>
2019-12-19 23:25:47 -0500

    updated get_new_comments and update_sentiment functions

--- hacker_sentiment.py
+++ hacker_sentiment.py
@@ -1,21 +1,32 @@
 import pandas as pd
 from datetime import datetime
 from .sentiment_model import score_sentiment
+from .get_user_posts import get_user_posts
 
 class User():
+
+
     def __init__(self, username):
         self.username = username
         cols = ['comment_id', 'text', 'sentiment']
-        scored_comments = pd.DataFrame(columns=cols)
-        update_sentiment(self.username)
-        last_update = datetime.now()
-        latest_comment_id = scored_comments.tail(1)['comment_id']
-        mean_sentiment = 0
-        
+        self.scored_comments = pd.DataFrame(columns=cols)
+        # update_sentiment(self.username)
+        self.last_update = datetime.now()
+        self.latest_comment_id = self.scored_comments.tail(1)['comment_id']
+        self.mean_sentiment = 0
+
+    def get_new_comments(self, username, last_loaded_comment=0):
+        """Returns pandas dataframe {'comment_id', 'text'} with id > last_loaded_comment"""
+        ids, posts = get_user_posts(username, limit=20)
+        comments_df = pd.DataFrame(list(zip(ids, posts)))
+        # Dummy return
+        # dummy_df = pd.DataFrame([[0,'dummy text', 0]], columns=self.cols)
+        return comments_df
+
 
     def update_sentiment(self):
         # Load new comments into df
-        new_comments_df = get_new_comments(self.username, self.latest_comment_id)
+        new_comments_df = self.get_new_comments(self.username)
 
         # Score new comments
         new_comments_df['sentiment'] = score_sentiment(new_comments_df['text'])
@@ -25,18 +36,9 @@ class User():
         self.latest_comment_id = self.scored_comments.tail(1)['comment_id']
         self.mean_sentiment = self.scored_comments['sentiment'].mean()
 
-    def get_new_comments(username, last_loaded_comment=0):
-        """Returns pandas dataframe {'comment_id', 'text'} with id > last_loaded_comment"""
-        # TODO: Implement
-
-        # Dummy return
-        dummy_df = pd.DataFrame([[0,'dummy text', 0]], columns=self.cols)
-        return dummy_df
-
-
     def get_saltiest_comment(self):
         """return comment with min sentiment"""
-        # TODO: Implement
+        # TODO: Implement, builds off get_new_comments
 
         # Dummy return
         dummy_comment = [0, 'dummy saltiest text', 0]
