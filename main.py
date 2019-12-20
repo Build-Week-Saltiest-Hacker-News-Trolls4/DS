50d3ba09f8eebca574e2ee61d84a1f896eccf8f9
Adrian du Plessis <a3an.at@gmail.com>
2019-12-20 10:51:42 -0600

    renamed _sentiment to _user and updated refs to it

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Main application logic goes here
+import hacker_access
+
+
+top_users = hacker_access.get_user_list()
+
+# Build top n users table
+for username in top_users:
+
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import hacker_access
-import hacker_sentiment
+import hacker_user
 import sentiment_model
 import main
 import heroku_pass_off
