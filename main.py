0df3df07f223d74bf33349b4ad77bf936825db4f
Adrian du Plessis <a3an.at@gmail.com>
2019-12-20 18:29:51 -0600

    main logic implemented to produce deliverable db

--- main.py
+++ main.py
@@ -5,18 +5,33 @@ from hacker_user import User
 
 top_users_usernames = hacker_access.get_user_list()
 
-cols = ['rank', 'score', 'username', 
-        'saltiest_comment_text', 'saltiest_comment_id']
-top_users_report = pd.DataFrame(columns=cols)
+observations = []
 
 # Build top n users table
+# Debug: takes forever
+# for username in top_users_usernames:
+
 for username in top_users_usernames:
+
+    # Debug
+    print('Loading user: ', username)
+
     user = User(username)
     user.update_sentiment() # Consider calling as part of __init__
 
     mean_sentiment = user.mean_sentiment
     saltiest_comment = user.get_saltiest_comment()
-    saltiest_comment_text = 
+
+    user_report = [mean_sentiment, username, saltiest_comment['text'], saltiest_comment['id']]
+    observations.append(user_report)
+
+headers = ['score', 'username', 
+        'saltiest_comment_text', 'saltiest_comment_id']
+
+top_users_report = pd.DataFrame(observations, columns=headers)
+
+print('works till here')
+print(top_users_report)
 
 
 
