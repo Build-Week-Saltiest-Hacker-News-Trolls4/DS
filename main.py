95b7d628217bb4faeb2e3e0768f74532b1f69187
Tally Wiesenberg <t.j.wies@gmail.com>
2020-01-07 22:41:31 -0500

    main.py made into one function

--- main.py
+++ main.py
@@ -3,32 +3,38 @@ import hacker_access
 import pandas as pd
 from hacker_user import User
 from heroku_pass_off import push_heroku
+def main_process():
+    df_by_comments = hacker_access.get_posts_and_users()
+    # df_by_users = hacker_access.update(df_by_comments)
+    observations = []
 
-users_usernames = hacker_access.get_user_list()
-observations = []
+    # df.sort_values('comment_id')
 
-for username in users_usernames[0:20]:
+    for username in df_by_comments.sort_values(by='sentiment', ascending=False)['username']:
 
-    print('Loading user: ', username) # Debug use only, delete.
+        print('Loading user: ', username) # Debug use only, delete.
 
-    user = User(username)
+        user = User(username)
 
-    mean_sentiment = user.mean_sentiment
-    saltiest_comment = user.get_saltiest_comment()
+        mean_sentiment = user.mean_sentiment
+        saltiest_comment = user.get_saltiest_comment()
 
-    user_report = [mean_sentiment, username, saltiest_comment['text'], saltiest_comment['comment_id']]
-    observations.append(user_report)
+        saltiest_comment_text = saltiest_comment['text'].to_string(index=False)
+        saltiest_comment_id = saltiest_comment['comment_id'].to_string(index=False)
 
-headers = ['score', 'username', 
-        'saltiest_comment_text', 'saltiest_comment_id']
+        user_report = [mean_sentiment, username, saltiest_comment_text, saltiest_comment_id]
+        observations.append(user_report)
 
-users_report = pd.DataFrame(observations, columns=headers)
-users_report = users_report.sort_values(by=['score'])
-users_report = users_report.reset_index(drop=True)
-top_20_table = users_report.head(20)
+    headers = ['score', 'username', 
+            'saltiest_comment_text', 'saltiest_comment_id']
 
-print(f'passing to heroku: \n{top_20_table}')
-push_heroku(top_20_table)
+    users_report = pd.DataFrame(observations, columns=headers)
+    users_report = users_report.sort_values(by=['score'])
+    users_report = users_report.reset_index(drop=True)
+    top_20_table = users_report.head(20)
+
+    print(f'passing to heroku: \n{top_20_table}')
+    push_heroku(top_20_table)
 
 
 
