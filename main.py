1bd900ce7f880f140493362a6f9ff5a5bfee8588
Adrian du Plessis <a3an.at@gmail.com>
2020-01-11 13:46:10 -0600

    Fixed drop columns syntax error

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ headers = ['score', 'username',
 print(df_by_users)
 users_report = df_by_users.rename(columns={"avg_score": "score", "user": "username",
                                   "saltiest_comment": "saltiest_comment_text"})
-users_report.drop(['num_comments', 'saltiest_comment_sentiment'], inplace=True)
+print(users_report)
+users_report.drop(['num_comments', 'saltiest_comment_sentiment'], axis='columns' inplace=True)
 users_report = users_report.sort_values(by=['score'])
 users_report = users_report.reset_index(drop=True)
 top_20_table = users_report.head(20)
